6fa596906595383e2fa8daac6aa5868153bcd95b
Russell Sim <russell.sim@gmail.com>
2022-04-18 22:20:11 +0200

    doc: update commentary

--- cue-mode.el
+++ cue-mode.el
@@ -20,7 +20,12 @@
 
 ;;; Commentary:
 
+;; Provides Emacs font-lock, indentation, and some useful functions
+;; for the CUE data validation language.
 ;;
+;; This mode has the following keybindings:
+;;   'C-c C-c' evaluate the current buffer and show the output
+;;   'C-c C-r' reformat the current buffer
 
 ;;; Code:
 
